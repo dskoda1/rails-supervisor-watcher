37c541c52206b1a2157cc67aff1cc4ec0d2a5484
David Skoda <dskoda1@binghamton.edu>
2017-02-05 04:18:10 +0000

    add login to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+    login
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
