62e296531dada94a748cf6253befe72ce67aea8b
David Skoda <dskoda1@binghamton.edu>
2017-02-05 04:20:30 +0000

    update static pages tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     login
   end
+ 
+  test "logged out should redirect" do
+    logout
+    get static_pages_home_url
+    assert_redirected_to :new_user_session
+  end
   
   test "should get home" do
     get static_pages_home_url
